db09e8e4e736d24c7ca08c1ad46aba5c707857e6
Kasra <kasra.majbouri@appscore.com.au>
2020-03-01 09:37:47 +1100

    Start coding by creating index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    	<ul>
+			<li> Get user input </li>
+			<li> Validate the user input </li>
+			<li> If everything is okay, then extract int codes </li>
+			<li> Do the calculation </li>
+			<li> Display the results </li>
+		</ul>
+
+    </div>
+  </div>
+</div>
+
+
