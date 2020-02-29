6cf87cf0bb0e66a0828b4f9fcd3df6d4c511b93d
Kasra <kasra.majbouri@appscore.com.au>
2020-03-01 09:56:45 +1100

    Include bootstrap, header and footer. Add a simple form to get user input

--- index.php
+++ index.php
@@ -1,19 +1,54 @@
 <?php
+   require_once('includes/header.php');
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-    	<ul>
-			<li> Get user input </li>
-			<li> Validate the user input </li>
-			<li> If everything is okay, then extract int codes </li>
-			<li> Do the calculation </li>
-			<li> Display the results </li>
-		</ul>
+		<div class="container">
+		  <div class="row">
+		    <div class="col-sm">
+		    	<ul>
+					<li> Get the user input </li>
+					<li> Validate the user input </li>
+					<li> If everything is okay, then analyze the commands </li>
+					<li> Do the calculation </li>
+					<li> Display the results </li>
+				</ul>
 
-    </div>
-  </div>
-</div>
+		    </div>
+		  </div>
+		</div>
 
 
+		<div class="container">
+
+				<form action="" method="POST">
+				  <div class="form-group">
+				  	<div class="col-xs-3">
+					    <label for="exampleInputEmail1">Please enter the Intcode program: </label>
+					    <input type="text" class="form-control" name="intcode" placeholder='Enter your IntCode'>
+					</div>
+				  </div>
+
+				  <button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+
+
+		</div>
+		<br>
+		<br>
+		<br>
+		<div class="container">
+			<?php
+
+				if(isset($_POST['intcode'])){ 
+				  	$userInput        = $_POST['intcode']; 
+
+					var_dump($userInput);
+				}  
+
+			?>
+
+		</div>
+
+	<?php
+	require_once('includes/footer.php');
+	?>			
